6f16aa097bc26ee8421df92a58f4f9c044ee27ef
cigsekaya <cigsekaya22@gmail.com>
2024-12-21 15:22:38 +0300

    signin page created

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,43 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:tedix/common/styles/spacing_styles.dart';
-import 'package:tedix/utils/constants/image_string.dart';
+import 'package:tedix/features/authentication/screens/signup/signup.dart';
 import 'package:tedix/utils/constants/sizes.dart';
 import 'package:tedix/utils/constants/text_strings.dart';
-import 'package:tedix/utils/helpers/helper_functions.dart';
+import 'package:get/get.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: TSpacingStyle.paddingWithAppBarHeight,
           child: Column(
             children: [
-              /// Logo, Title & Sub-Title
+
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(TTexts.loginTitle,
                       style: Theme.of(context).textTheme.headlineMedium),
                   const SizedBox(height: TSizes.sm),
-                  Text(TTexts.loginSubTitle,
+                  Text(TTexts.loginSubTitle,//
                       style: Theme.of(context).textTheme.bodyMedium),
                 ],
               ),
 
-              /// Form
+
               Form(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
                       vertical: TSizes.spaceBtwSections),
                   child: Column(
                     children: [
-                      /// Email
+
                       TextFormField(
                         decoration: const InputDecoration(
                             prefixIcon: Icon(Iconsax.direct_right),
@@ -45,7 +45,7 @@ class LoginScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: TSizes.spaceBtwInputFields),
 
-                      /// Password
+
                       TextFormField(
                         decoration: const InputDecoration(
                           prefixIcon: Icon(Iconsax.password_check),
@@ -57,7 +57,7 @@ class LoginScreen extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          /// Forget Password
+
                           TextButton(
                               onPressed: () {},
                               child: const Text(TTexts.forgetPassword)),
@@ -65,7 +65,7 @@ class LoginScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: TSizes.spaceBtwSections),
 
-                      /// Sign In Button
+
                       SizedBox(
                           width: double.infinity,
                           child: ElevatedButton(
@@ -73,12 +73,13 @@ class LoginScreen extends StatelessWidget {
                               child: const Text(TTexts.signIn))),
                       const SizedBox(height: TSizes.spaceBtwSections),
 
-                      /// Create Account Button
+
                       SizedBox(
-                          width: double.infinity,
-                          child: OutlinedButton(
-                              onPressed: () {},
-                              child: const Text(TTexts.createAccount))),
+                        width: double.infinity,
+                        child: OutlinedButton(
+                            onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context)=> const SignupScreen()));},
+                            child: const Text(TTexts.createAccount)),
+                      ),
                       const SizedBox(height: TSizes.spaceBtwSections),
                     ],
                   ),
---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:tedix/utils/constants/sizes.dart';
+import 'package:tedix/utils/constants/text_strings.dart';
+import 'package:iconsax/iconsax.dart';
+
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(TTexts.signupTitle, style: Theme.of(context).textTheme.headlineMedium),
+                const SizedBox(height: TSizes.spaceBtwSections),
+
+                Form(
+                  child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: TTexts.firstName,prefixIcon: Icon(Iconsax.user),
+
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: TSizes.spaceBtwInputFields),
+
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: TTexts.lastName,prefixIcon: Icon(Iconsax.user),
+                            ),
+                          ),
+                        ),
+                       //const SizedBox(width: TSizes.spaceBtwInputFields),
+                    ],
+                    ),
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+
+                        TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(
+                              labelText: TTexts.username,prefixIcon: Icon(Iconsax.user_edit),
+                            ),
+                          ),
+
+                    const SizedBox(height: TSizes.spaceBtwInputFields),
+                        const SizedBox(width: TSizes.spaceBtwInputFields),
+
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: TTexts.email,
+                              prefixIcon: Icon(Iconsax.direct),
+                            ),
+                          ),
+
+
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: TTexts.phoneNo,
+                              prefixIcon: Icon(Iconsax.call),
+                            ),
+                          ),
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+
+
+
+                           TextFormField(
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: TTexts.password,
+                              prefixIcon: Icon(Iconsax.password_check),
+                              suffixIcon: Icon(Iconsax.eye_slash),
+                            ),
+                          ),
+
+
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+                    //const SizedBox(height: TSizes.spaceBtwSections),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child:ElevatedButton(onPressed:(){},child: const Text(TTexts.createAccount),),
+                    ),
+
+                  ],
+                  ),
+                ),
+            ],
+            ),
+          ),
+      ),
+    );
+  }
+}
